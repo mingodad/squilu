d741b7b403ed9df450a9bd8b38eaeffe14f5ebc1
mingodad <mingodad@gmail.com>
2012-12-27 17:19:03 +0000

    Add some extensions to squirrel sq_axtls is to provide a glue with the lightweight AXTLS library sqbase64 is a port of lbase64 from a Lua extension sq_sqlite3 is an sqlite3 module to squirrel sqmix is a port of luamix template module for Lua

---
+++ ext/code_mix_prep.c
@@ -0,0 +1,355 @@
+/* Copyright (c) 2007-2008 Thomas Lavergne
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Adapted by Domingo Alvarez Duarte
+ */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef MixInteger
+#define MixInteger int
+#endif
+
+#define mix_tempsize 4096
+
+static const char *err_badeof = "unexpected end of file";
+
+enum mix_token_e {
+	tok_sh,
+	tok_code_start, tok_code_end, tok_expr_start, tok_expr_end,
+	tok_data, tok_code, tok_expr,
+	tok_eof
+};
+
+typedef struct mix_state_s {
+	size_t      size;
+	const char *buffer;
+
+	const char *code_start;
+	size_t      code_startsize;
+	const char *code_end;
+	size_t      code_endsize;
+	const char *expr;
+	size_t      exprsize;
+	char       print_out[64];
+	size_t      print_outsize;
+
+	size_t      pos;
+	int         token;
+	char        temp[mix_tempsize];
+
+	char        sq_reader_buf[mix_tempsize];
+	char        *sq_reader_last_pos;
+	char        *sq_reader_pos;
+
+	const char *error;
+	MixInteger result_size, result_pos;
+	const char *result;
+	size_t new_lines;
+} mix_state_t;
+
+/* Read an open bracket of the form [=*[ and return the number of = found.
+ * if there is no string like this at the current pos return -1.
+ * after this call the cursor is set at the first char after the bracket.
+ */
+static int mix_bracket_open(mix_state_t *S) {
+	size_t pos, cnt;
+	if (S->buffer[S->pos] != '[')
+		return -1;
+	pos = S->pos + 1;
+	cnt = 0;
+	while (pos < S->size && S->buffer[pos] == '=')
+		++pos, ++cnt;
+	if (pos == S->size || S->buffer[pos] != '[')
+		return -1;
+	S->pos = pos + 1;
+	return cnt;
+}
+
+/* Read a close bracket of the form ]=*] and return the number of = found.
+ * if there is no string like this at the current pos return -1.
+ * after this call the cursor is set at the first char after the bracket.
+ */
+static int mix_bracket_close(mix_state_t *S) {
+	size_t pos, cnt;
+	if (S->buffer[S->pos] != ']')
+		return -1;
+	pos = S->pos + 1;
+	cnt = 0;
+	while (pos < S->size && S->buffer[pos] == '=')
+		++pos, ++cnt;
+	if (pos == S->size || S->buffer[pos] != ']')
+		return -1;
+	S->pos = pos + 1;
+	return cnt;
+}
+
+/* Skip a lua string enclosed by type (who can be either \" or \') taking
+ * account of escape chars. after a call to this the cursor is set at the
+ * first character after the string.
+ * Beware that on enter the cursor must be on the first char of the string
+ * not on the openning char.
+ */
+static int mix_skip_string(mix_state_t *S, char type) {
+	while (S->pos < S->size) {
+		if (S->buffer[S->pos] == type) {
+			++S->pos;
+			return 0;
+		}
+		if (S->buffer[S->pos] == '\\')
+			++S->pos;
+		++S->pos;
+	}
+	return -1;
+}
+
+/* Skip a lua long string wich is enclosed by square bracket and level = signs
+ * after the call the cursor is set on the first character after the string.
+ * Beware that on enter the cursor must be on the first char of the string
+ * not on the openning bracket.
+ */
+static int mix_skip_lstring(mix_state_t *S, int level) {
+	while (S->pos < S->size) {
+		int tmp = mix_bracket_close(S);
+		if (tmp == -1)
+			++S->pos;
+		else if (tmp == level)
+			return 0;
+	}
+	return -1;
+}
+
+/* Skip a squirrel comment either one line or long. after the call the cursor is set
+ * to the first character after the coment.
+ * Beware that on enter the cursor must be set on the first char after the //
+ * comment prefix.
+ */
+static int mix_skip_comment(mix_state_t *S) {
+	int level = mix_bracket_open(S);
+	if (level != -1)
+		return mix_skip_string(S, level);
+	while (S->pos < S->size && S->buffer[S->pos] != '\n')
+		++S->pos;
+    ++S->new_lines;
+    return 0;
+}
+
+#define mix_iscode_start(s) (!strncmp((s)->code_start, (s)->buffer + (s)->pos, (s)->code_startsize))
+#define mix_iscode_end(s) (!strncmp((s)->code_end, (s)->buffer + (s)->pos, (s)->code_endsize))
+#define mix_isexpr(s) (!strncmp((s)->expr, (s)->buffer + (s)->pos, (s)->exprsize))
+
+#define result_literal(s) S->result_size = sizeof(s)-1, S->result = s;
+
+static const char * sq_mix_reader_str(void *ud) {
+	mix_state_t *S = (mix_state_t *)ud;
+
+	if (S->error != NULL)
+		return 0;
+
+	/* Data chunk are sent to lua by block cause we must escape some char
+	 * and we cannot modify the buffer itself. So we must use another
+	 * buffer sending escaped block one by one.
+	 * If there is no data to send in the chunk, change the state and go
+	 * directly to the tok_code_start part.
+	 */
+	if (S->token == tok_data) {
+		S->result_size = 0;
+		while (S->pos < S->size) {
+			char c = S->buffer[S->pos];
+			if (c == S->code_start[0])
+				if (mix_iscode_start(S))
+					break;
+			if (c == '\\' || c == '"')
+				S->temp[S->result_size++] = '\\';
+			if (c == '\r' || c == '\n'){
+				S->temp[S->result_size++] = '\\';
+				if(c == '\r') c = 'r';
+				else {
+				    S->new_lines++;
+				    c = 'n';
+				}
+            }
+
+			S->temp[S->result_size++] = c;
+			++S->pos;
+			if (S->result_size >= mix_tempsize - 1) {
+				if (S->pos == S->size)
+					S->token = tok_code_start;
+				return S->result = S->temp;
+			}
+		}
+		if (S->pos < S->size)
+			S->pos += S->code_startsize;
+		S->token = tok_code_start;
+		if (S->result_size != 0)
+			return S->result = S->temp;
+	}
+
+	/* Send the termination of the string and the function call before
+	 * going in tok_code/tok_expr state or if at eof go to tok_eof.
+	 */
+	if (S->token == tok_code_start) {
+		if (S->pos == S->size) {
+			S->token = tok_eof;
+		} else if (mix_isexpr(S)) {
+			S->token = tok_expr_start;
+			S->pos += S->exprsize;
+		} else {
+			S->token = tok_code;
+		}
+		return result_literal("\");");
+	}
+
+	/* Send the output function, but without the string start character, so
+	 * the result of the lua code will be send as parameters to the output
+	 * function.
+	 */
+	if (S->token == tok_expr_start) {
+	    if(S->new_lines){
+	        --S->new_lines;
+	        return result_literal("\n");
+	    }
+		S->token = tok_expr;
+		S->result_size = S->print_outsize - 1;
+		return S->result = S->print_out;
+	}
+
+	/* Send a lua chunk in one block to the lua engine. No escaping are
+	 * needed here so we can send the full block. Then switch to the
+	 * tok_code_end. (this correctly skip all form of lua string and comments)
+	 * If no lua code is found, go directly to the tok_code_end part.
+	 */
+	if (S->token == tok_code || S->token == tok_expr) {
+	    if(S->new_lines){
+	        --S->new_lines;
+	        return result_literal("\n");
+	    }
+		size_t old = S->pos;
+		while (S->pos < S->size) {
+			char c = S->buffer[S->pos];
+			if (c == '\'') {
+				++S->pos;
+				mix_skip_string(S, '\'');
+			} else if (c == '"') {
+				++S->pos;
+				mix_skip_string(S, '"');
+			} else if (c == '[') {
+				int level = mix_bracket_open(S);
+				if (level != -1)
+					mix_skip_lstring(S, level);
+				else
+					++S->pos;
+			} else if (c == '/') {
+				++S->pos;
+				if (S->buffer[S->pos] == '/') {
+					++S->pos;
+					mix_skip_comment(S);
+				}
+			} else if (c == S->code_end[0]) {
+				if (mix_iscode_end(S))
+					break;
+				++S->pos;
+			} else {
+				++S->pos;
+			}
+		}
+		S->result_size = S->pos - old;
+		if (S->pos == S->size) {
+			S->error = err_badeof;
+			return NULL;
+		}
+		S->token = S->token == tok_code ? tok_code_end : tok_expr_end;
+		S->pos += S->code_endsize;
+		if (S->result_size != 0)
+			return S->result = S->buffer + old;
+	}
+
+	/* Send the data output function name and (' that start a data block to
+	 * the lua engine and switch to the tok_data state.
+	 */
+	if (S->token == tok_code_end) {
+	    if(S->new_lines){
+	        --S->new_lines;
+	        return result_literal("\n");
+	    }
+		S->token = tok_data;
+		S->result_size = S->print_outsize;
+		return S->result = S->print_out;
+	}
+
+	/* Close the output function call on end of expression and go to the lua
+	 * to data transition.
+	 */
+	if (S->token == tok_expr_end) {
+		S->token = tok_code_end;
+		return result_literal(");");
+	}
+
+	/* If we skipped a sh-bang line we must send an empty line to keep the
+	 * lua line counter correct.
+	 */
+	if (S->token == tok_sh) {
+		S->token = tok_code_end;
+		++S->new_lines;
+		return result_literal("\n");
+	}
+
+	return NULL;
+}
+
+static MixInteger sq_mix_reader_char(void *ud) {
+	mix_state_t *S = (mix_state_t *)ud;
+
+	if (S->error != NULL) return 0;
+start:
+	if (S->result_size){
+	    MixInteger c = S->result[S->result_pos++];
+	    if(S->result_pos >= S->result_size){
+	        S->result_pos = S->result_size = 0;
+	    }
+	    return c;
+	}
+	if(sq_mix_reader_str(ud)) goto start;
+	return 0;
+}
+
+static void sq_mix_init(mix_state_t *S,
+                        char *buffer, int buffer_size,
+                        const char *print_out,
+                        const char *code_start,
+                        const char *code_end,
+                        const char *expr){
+	memset(S, 0, sizeof(mix_state_t));
+	snprintf(S->print_out, sizeof(S->print_out), "%s(\"",  print_out ? print_out : "mix_write");
+	S->print_outsize = strlen(S->print_out);
+	S->code_start = code_start ? code_start : "{%";
+	S->code_startsize = strlen(S->code_start);
+	S->code_end = code_end ? code_end : "%}";
+	S->code_endsize = strlen(S->code_end);
+	S->expr = expr ? expr : "=";
+	S->exprsize = strlen(S->expr);
+	S->buffer = buffer;
+	S->size = buffer_size;
+	S->token = tok_code_end;
+}
+
---
+++ ext/sq_axtls.c
@@ -0,0 +1,461 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "squirrel.h"
+#include <stdlib.h>  /* for malloc */
+#include <assert.h>  /* for a few sanity tests */
+
+#define GET_INT(idx, var) int var; sq_getinteger(sqvm, idx, &var)
+#define GET_STR(idx, var) const SQChar *var; sq_getstring(sqvm, idx, &var)
+
+#include "ssl.h"
+
+#define SQ_NETLIBNAME "axtlsl"
+
+typedef struct {
+	SSL_CTX *ptr;
+} SSL_CTX_ptr;
+
+static const char *SSL_CTX_metaTag   = "sq_axtls";
+
+static SSL_CTX_ptr *sq_ssl_get_ctx(HSQUIRRELVM sqvm, int index) {
+    SSL_CTX_ptr *ssl_ctx_ptr = (SSL_CTX_ptr*)luaL_checkudata(sqvm, index, SSL_CTX_metaTag);
+    if (ssl_ctx_ptr == NULL)
+        luaL_argerror(sqvm, index, "bad SSL CONTEXT");
+    if (ssl_ctx_ptr->ptr == NULL)
+        luaL_argerror(sqvm, index, "SSL CONTEXT has NULL value");
+    return ssl_ctx_ptr;
+}
+
+
+typedef struct {
+	SSL *ptr;
+	int free_ptr_on_gc;
+} SSL_ptr;
+
+static const char *SSL_metaTag   = "axtls.ssl";
+
+static SSL_ptr *sq_ssl_get_ssl(HSQUIRRELVM sqvm, int index) {
+    SSL_ptr *ssl_ptr = (SSL_ptr*)luaL_checkudata(sqvm, index, SSL_metaTag);
+    if (ssl_ptr == NULL)
+        luaL_argerror(sqvm, index, "bad SSL");
+    if (ssl_ptr->ptr == NULL)
+        luaL_argerror(sqvm, index, "SSL has NULL value");
+    return ssl_ptr;
+}
+
+
+static int sq_ssl_ctx_new(HSQUIRRELVM sqvm){
+	int options = sq_getinteger(sqvm, 1);
+	int num_sessions = sq_getinteger(sqvm, 2);
+    SSL_CTX *ssl_ctx = ssl_ctx_new(options, num_sessions);
+    if(ssl_ctx) {
+        SSL_CTX_ptr *ssl_ctx_ptr = (SSL_CTX_ptr*)sq_newuserdata(sqvm, sizeof(SSL_CTX_ptr));
+        ssl_ctx_ptr->ptr = ssl_ctx;
+        luaL_newmetatable(sqvm, SSL_CTX_metaTag);
+        sq_setmetatable(sqvm, -2);        /* set metatable */
+    } else sq_pushnil(L);
+
+	return 1;
+}
+
+static int sq_ssl_ctx_free(HSQUIRRELVM sqvm){
+	SSL_CTX_ptr *ssl_ctx_ptr = sq_ssl_get_ctx(sqvm, 1);
+	if(ssl_ctx_ptr->ptr){
+		ssl_ctx_free(ssl_ctx_ptr->ptr);
+		ssl_ctx_ptr->ptr = NULL;
+	}
+	return 0;
+}
+
+static int sq_ssl_server_new(HSQUIRRELVM sqvm){
+	SSL_CTX_ptr *ssl_ctx_ptr = sq_ssl_get_ctx(sqvm, 1);
+	int client_fd = sq_getinteger(sqvm, 2);
+    SSL *ssl = ssl_server_new(ssl_ctx_ptr->ptr, client_fd);
+
+    if(ssl) {
+        SSL_ptr *ssl_ptr = (SSL_ptr*)sq_newuserdata(sqvm, sizeof(SSL_ptr));
+        ssl_ptr->ptr = ssl;
+        ssl_ptr->free_ptr_on_gc = 1;
+
+        luaL_newmetatable(sqvm, SSL_metaTag);
+        sq_setmetatable(sqvm, -2);        /* set metatable */
+    } else sq_pushnil(L);
+
+	return 1;
+}
+
+static int sq_ssl_client_new(HSQUIRRELVM sqvm){
+	SSL_CTX_ptr *ssl_ctx_ptr = sq_ssl_get_ctx(sqvm, 1);
+	int client_fd = sq_getinteger(sqvm, 2);
+	uint8_t *session_id = (uint8_t *)luaL_optstring(sqvm, 3, NULL);
+	uint8_t sess_id_size= luaL_optint(sqvm, 4, 0);
+	SSL *ssl = ssl_client_new(ssl_ctx_ptr->ptr, client_fd, session_id, sess_id_size);
+
+    if(ssl) {
+        SSL_ptr *ssl_ptr = (SSL_ptr*)sq_newuserdata(sqvm, sizeof(SSL_ptr));
+        ssl_ptr->ptr = ssl;
+        ssl_ptr->free_ptr_on_gc = 1;
+
+        luaL_newmetatable(sqvm, SSL_metaTag);
+        sq_setmetatable(sqvm, -2);        /* set metatable */
+    } else sq_pushnil(L);
+
+	return 1;
+}
+
+static int sq_ssl_free(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	if(ssl_ptr->ptr && ssl_ptr->free_ptr_on_gc){
+		ssl_free(ssl_ptr->ptr);
+		ssl_ptr->ptr = NULL;
+	}
+	return 0;
+}
+
+static int sq_ssl_read(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	uint8_t *in_data;
+	int result = ssl_read(ssl_ptr->ptr, &in_data);
+	sq_pushinteger(sqvm, result);
+	int return_params = 1;
+	if (result > SSL_OK) {
+	  sq_pushlstring(sqvm, (char *)in_data, result);
+      return_params++;
+    }
+	return return_params;
+}
+
+static int sq_ssl_write(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	uint8_t *out_data;
+	sq_getstring(sqvm, 2, &out_data);
+	int out_len;
+	if(sq_gettop(sqvm) > 2) sq_getinteger(sqvm, 3, &out_len);
+	else out_len = sq_getsize(sqvm, 2);
+	sq_pushinteger(sqvm, ssl_write(ssl_ptr->ptr, out_data, out_len));
+	return 1;
+}
+
+static int sq_ssl_find(HSQUIRRELVM sqvm){
+	SSL_CTX_ptr *ssl_ctx_ptr = sq_ssl_get_ctx(sqvm, 1);
+	GET_INT(2, client_fd);
+    SSL *ssl = ssl_find(ssl_ctx_ptr->ptr, client_fd);
+
+    if(ssl){
+        SSL_ptr *ssl_ptr = (SSL_ptr*)sq_newuserdata(sqvm, sizeof(SSL_ptr));
+        ssl_ptr->ptr = ssl;
+        //we don't want to free this SSL when collecting garbage
+        ssl_ptr->free_ptr_on_gc = 0;
+
+        luaL_newmetatable(sqvm, SSL_metaTag);
+        sq_setmetatable(sqvm, -2);        /* set metatable */
+    } else sq_pushnil(L);
+
+	return 1;
+}
+
+static int sq_ssl_get_session_id(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	const uint8_t * result = ssl_get_session_id(ssl_ptr->ptr);
+    sq_pushlstring(sqvm, (char *)result, ssl_get_session_id_size(ssl_ptr->ptr));
+	return 1;
+}
+
+static int sq_ssl_get_session_id_size(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	uint8_t result = ssl_get_session_id_size(ssl_ptr->ptr);
+	sq_pushinteger(sqvm, result);
+	return 1;
+}
+
+static int sq_ssl_get_cipher_id(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	uint8_t result = ssl_get_cipher_id(ssl_ptr->ptr);
+	sq_pushinteger(sqvm, result);
+	return 1;
+}
+
+static int sq_ssl_handshake_status(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	int result = ssl_handshake_status(ssl_ptr->ptr);
+	sq_pushinteger(sqvm, result);
+	return 1;
+}
+
+static int sq_ssl_get_config(HSQUIRRELVM sqvm){
+    GET_INT(1, info);
+	sq_pushinteger(sqvm, ssl_get_config(info));
+	return 1;
+}
+
+static int sq_ssl_display_error(HSQUIRRELVM sqvm){
+    GET_INT(1, error);
+	ssl_display_error(error);
+	return 0;
+}
+
+static int sq_ssl_verify_cert(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	int result = ssl_verify_cert(ssl_ptr->ptr);
+	sq_pushinteger(sqvm, result);
+	return 1;
+}
+
+static int sq_ssl_get_cert_dn(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	GET_INT(2, component);
+	const char* result = ssl_get_cert_dn(ssl_ptr->ptr, component);
+	sq_pushstring(sqvm, result);
+	return 1;
+}
+
+static int sq_ssl_get_cert_subject_alt_dnsname(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	GET_INT(2, dnsindex);
+	const char* result = ssl_get_cert_subject_alt_dnsname(ssl_ptr->ptr, dnsindex);
+	sq_pushstring(sqvm, result);
+	return 1;
+}
+
+static int sq_ssl_renegotiate(HSQUIRRELVM sqvm){
+	SSL_ptr *ssl_ptr = sq_ssl_get_ssl(sqvm, 1);
+	int result = ssl_renegotiate(ssl_ptr->ptr);
+	sq_pushinteger(sqvm, result);
+	return 1;
+}
+
+static int sq_ssl_obj_load(HSQUIRRELVM sqvm){
+	SSL_CTX_ptr *ssl_ctx_ptr = sq_ssl_get_ctx(sqvm, 1);
+	GET_INT(2, obj_type);
+	GET_STR(3, filename);
+	GET_STR(4, password);
+	int result = ssl_obj_load(ssl_ctx_ptr->ptr, obj_type,filename,password);
+	sq_pushinteger(sqvm, result);
+	return 1;
+}
+
+static int sq_ssl_obj_memory_load(HSQUIRRELVM sqvm){
+	SSL_CTX_ptr *ssl_ctx_ptr = sq_ssl_get_ctx(sqvm, 1);
+	GET_INT(2, obj_type);
+	uint8_t *data;
+	sq_getstring(sqvm, 3, &data);
+	GET_INT(4, data_len);
+	GET_STR(5, password);
+	int result = ssl_obj_memory_load(ssl_ctx_ptr->ptr, obj_type,data,data_len, password);
+	sq_pushinteger(sqvm, result);
+	return 1;
+}
+
+static int sq_ssl_version(HSQUIRRELVM sqvm){
+	sq_pushstring(sqvm,(const char*)ssl_version());
+	return 1;
+}
+
+static SQInteger ssl_ctx_release_hook(SQUserPointer p, SQInteger size)
+{
+	ssl_ctx_free((SSL_CTX *)p);
+	return 1;
+}
+
+static SQInteger ssl_ctx_constructor(HSQUIRRELVM sqvm)
+{
+	SQInteger options, num_sessions;
+    sq_getinteger(sqvm, 2, &options);
+    sq_getinteger(sqvm, 3, &num_sessions);
+
+	SSL_CTX *ssl_ctx = ssl_ctx_new(options, num_sessions);
+    if(!ssl_ctx)
+        return sq_throwerror(sqvm, _SC("Could'nt create an ssl context."))
+
+    sq_setinstanceup(sqvm, 1, ssl_ctx);
+    RELEASE_HOOK(sq_setreleasehook(sqvm,1, ssl_ctx_release_hook));
+	return 1;
+}
+
+static SQInteger ssl_release_hook(SQUserPointer p, SQInteger size)
+{
+	ssl_free((SSL *)p);
+	return 1;
+}
+
+static SQInteger ssl_constructor(HSQUIRRELVM sqvm)
+{
+	SQInteger socket_fd, num_sessions;
+    sq_getinteger(sqvm, 2, &options);
+    sq_getinteger(sqvm, 3, &num_sessions);
+
+	SSL *ssl = ssl_new(options, num_sessions);
+    if(!ssl)
+        return sq_throwerror(sqvm, _SC("Could'nt create an ssl server/client."))
+
+    sq_setinstanceup(sqvm, 1, ssl);
+    RELEASE_HOOK(sq_setreleasehook(sqvm,1, ssl_release_hook));
+	return 1;
+}
+
+#define _DECL_FL_FUNC(name,nparams,pmask) {_SC(#name),sq_##name,nparams,pmask}
+static SQRegFunction ssl_ctx_obj_funcs[]={
+	_DECL_FL_BOX_FUNC(constructor,3,_SC("xii")),
+	_DECL_FL_FUNC(server_new,2,_SC("xx")),
+	_DECL_FL_FUNC(client_new,2,_SC("xxii")),
+	_DECL_FL_FUNC(find,2,_SC("tu")),
+	_DECL_FL_FUNC(get_session_id,2,_SC("tu")),
+	_DECL_FL_FUNC(get_session_id_size,2,_SC("tu")),
+	_DECL_FL_FUNC(get_cipher_id,2,_SC("tu")),
+	_DECL_FL_FUNC(handshake_status,2,_SC("tu")),
+	_DECL_FL_FUNC(get_config,2,_SC("tu")),
+	_DECL_FL_FUNC(display_error,2,_SC("tu")),
+	_DECL_FL_FUNC(verify_cert,2,_SC("tu")),
+	_DECL_FL_FUNC(get_cert_dn,2,_SC("tu")),
+	_DECL_FL_FUNC(get_cert_subject_alt_dnsname,2,_SC("tu")),
+	_DECL_FL_FUNC(renegotiate,2,_SC("tu")),
+	_DECL_FL_FUNC(obj_load,2,_SC("tu")),
+	_DECL_FL_FUNC(obj_memory_load,2,_SC("tu")),
+	_DECL_FL_FUNC(version,2,_SC("tu")),
+	{0,0}
+};
+#undef _DECL_FL_FUNC
+
+#define _DECL_FL_FUNC(name,nparams,pmask) {_SC(#name),sq_##name,nparams,pmask}
+static SQRegFunction ssl_obj_funcs[]={
+	_DECL_FL_FUNC(ctx_new,3,_SC("tii")),
+	_DECL_FL_FUNC(ctx_free,2,_SC("tu")),
+	_DECL_FL_FUNC(server_new,2,_SC("tu")),
+	_DECL_FL_FUNC(client_new,2,_SC("tu")),
+	_DECL_FL_FUNC(free,2,_SC("tu")),
+	_DECL_FL_FUNC(read,2,_SC("tu")),
+	_DECL_FL_FUNC(write,2,_SC("tu")),
+	_DECL_FL_FUNC(find,2,_SC("tu")),
+	_DECL_FL_FUNC(get_session_id,2,_SC("tu")),
+	_DECL_FL_FUNC(get_session_id_size,2,_SC("tu")),
+	_DECL_FL_FUNC(get_cipher_id,2,_SC("tu")),
+	_DECL_FL_FUNC(handshake_status,2,_SC("tu")),
+	_DECL_FL_FUNC(get_config,2,_SC("tu")),
+	_DECL_FL_FUNC(display_error,2,_SC("tu")),
+	_DECL_FL_FUNC(verify_cert,2,_SC("tu")),
+	_DECL_FL_FUNC(get_cert_dn,2,_SC("tu")),
+	_DECL_FL_FUNC(get_cert_subject_alt_dnsname,2,_SC("tu")),
+	_DECL_FL_FUNC(renegotiate,2,_SC("tu")),
+	_DECL_FL_FUNC(obj_load,2,_SC("tu")),
+	_DECL_FL_FUNC(obj_memory_load,2,_SC("tu")),
+	_DECL_FL_FUNC(version,2,_SC("tu")),
+	{0,0}
+};
+#undef _DECL_FL_FUNC
+
+typedef struct {
+  const SQChar *Str;
+  SQInteger Val;
+} KeyIntType, * KeyIntPtrType;
+
+static KeyIntType axtls_constants[] = {
+    #define MK_CONST(c) {_SC(#c), c}
+    MK_CONST(SSL_SESSION_ID_SIZE),
+    MK_CONST(SSL_CLIENT_AUTHENTICATION),
+    MK_CONST(SSL_SERVER_VERIFY_LATER),
+    MK_CONST(SSL_NO_DEFAULT_KEY),
+    MK_CONST(SSL_DISPLAY_STATES),
+    MK_CONST(SSL_DISPLAY_BYTES),
+    MK_CONST(SSL_DISPLAY_CERTS),
+    MK_CONST(SSL_DISPLAY_RSA),
+    MK_CONST(SSL_CONNECT_IN_PARTS),
+    MK_CONST(SSL_OK),
+    MK_CONST(SSL_NOT_OK),
+    MK_CONST(SSL_ERROR_DEAD),
+    MK_CONST(SSL_CLOSE_NOTIFY),
+    MK_CONST(SSL_ERROR_CONN_LOST),
+    MK_CONST(SSL_ERROR_SOCK_SETUP_FAILURE),
+    MK_CONST(SSL_ERROR_INVALID_HANDSHAKE),
+    MK_CONST(SSL_ERROR_INVALID_PROT_MSG),
+    MK_CONST(SSL_ERROR_INVALID_HMAC),
+    MK_CONST(SSL_ERROR_INVALID_VERSION),
+    MK_CONST(SSL_ERROR_INVALID_SESSION),
+    MK_CONST(SSL_ERROR_NO_CIPHER),
+    MK_CONST(SSL_ERROR_BAD_CERTIFICATE),
+    MK_CONST(SSL_ERROR_INVALID_KEY),
+    MK_CONST(SSL_ERROR_FINISHED_INVALID),
+    MK_CONST(SSL_ERROR_NO_CERT_DEFINED),
+    MK_CONST(SSL_ERROR_NO_CLIENT_RENOG),
+    MK_CONST(SSL_ERROR_NOT_SUPPORTED),
+    MK_CONST(SSL_X509_OFFSET),
+    MK_CONST(SSL_ALERT_TYPE_WARNING),
+    MK_CONST(SLL_ALERT_TYPE_FATAL),
+    MK_CONST(SSL_ALERT_CLOSE_NOTIFY),
+    MK_CONST(SSL_ALERT_UNEXPECTED_MESSAGE),
+    MK_CONST(SSL_ALERT_BAD_RECORD_MAC),
+    MK_CONST(SSL_ALERT_HANDSHAKE_FAILURE),
+    MK_CONST(SSL_ALERT_BAD_CERTIFICATE),
+    MK_CONST(SSL_ALERT_ILLEGAL_PARAMETER),
+    MK_CONST(SSL_ALERT_DECODE_ERROR),
+    MK_CONST(SSL_ALERT_DECRYPT_ERROR),
+    MK_CONST(SSL_ALERT_INVALID_VERSION),
+    MK_CONST(SSL_ALERT_NO_RENEGOTIATION),
+    MK_CONST(SSL_AES128_SHA),
+    MK_CONST(SSL_AES256_SHA),
+    MK_CONST(SSL_RC4_128_SHA),
+    MK_CONST(SSL_RC4_128_MD5),
+    MK_CONST(SSL_BUILD_SKELETON_MODE),
+    MK_CONST(SSL_BUILD_SERVER_ONLY),
+    MK_CONST(SSL_BUILD_ENABLE_VERIFICATION),
+    MK_CONST(SSL_BUILD_ENABLE_CLIENT),
+    MK_CONST(SSL_BUILD_FULL_MODE),
+    MK_CONST(SSL_BUILD_MODE),
+    MK_CONST(SSL_MAX_CERT_CFG_OFFSET),
+    MK_CONST(SSL_MAX_CA_CERT_CFG_OFFSET),
+    MK_CONST(SSL_HAS_PEM),
+    MK_CONST(SSL_DEFAULT_SVR_SESS),
+    MK_CONST(SSL_DEFAULT_CLNT_SESS),
+    MK_CONST(SSL_X509_CERT_COMMON_NAME),
+    MK_CONST(SSL_X509_CERT_ORGANIZATION),
+    MK_CONST(SSL_X509_CERT_ORGANIZATIONAL_NAME),
+    MK_CONST(SSL_X509_CA_CERT_COMMON_NAME),
+    MK_CONST(SSL_X509_CA_CERT_ORGANIZATION),
+    MK_CONST(SSL_X509_CA_CERT_ORGANIZATIONAL_NAME),
+    MK_CONST(SSL_OBJ_X509_CERT),
+    MK_CONST(SSL_OBJ_X509_CACERT),
+    MK_CONST(SSL_OBJ_RSA_KEY),
+    MK_CONST(SSL_OBJ_PKCS8),
+    MK_CONST(SSL_OBJ_PKCS12),
+    {0,0}
+};
+
+/* This defines a function that opens up your library. */
+SQRESULT sq_register_axtlsl (HSQUIRRELVM sqvm) {
+    //add constants
+    sq_pushconsttable(sqvm);    //get the constants table first
+    for (KeyIntPtrType KeyIntPtr = axtls_constants; KeyIntPtr->Str; KeyIntPtr++) {
+        sq_pushstring(sqvm, KeyIntPtr->Str, -1);    //first the key
+        sq_pushinteger(sqvm, KeyIntPtr->Val);       //then the value
+        sq_newslot(sqvm, -3, SQFalse);              //store then
+    }
+    sq_poptop(sqvm); //pop remove constants table when we finished
+
+    //add a namespace axtls
+	sq_pushstring(sqvm,_SC("axtls"),-1);
+	sq_newtable(sqvm);
+
+    //now create the SSL Context class
+	sq_pushstring(sqvm,_SC("ssl_ctx"),-1);
+	sq_newclass(sqvm,SQFalse);
+	sq_settypetag(sqvm,-1,(void*)SSL_CTX_TAG(Fl));
+	insertFuncs(sqvm, ssl_ctx_obj_funcs);
+	sq_newslot(sqvm,-3,SQFalse);
+
+    //now create the SSL class
+	sq_pushstring(sqvm,_SC("ssl"),-1);
+	sq_newclass(sqvm,SQFalse);
+	sq_settypetag(sqvm,-1,(void*)SSL_TAG(Fl));
+	insertFuncs(sqvm, ssl_obj_funcs);
+	sq_newslot(sqvm,-3,SQFalse);
+
+	sq_newslot(sqvm,-3,SQFalse); //add axtls table to the root table
+    sq_poptop(sqvm); //removes axtls table
+
+    return SQ_OK;
+}
+
+#ifdef __cplusplus
+}
+#endif
