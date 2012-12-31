77aea3f62e1c93339b067c5880b95f93028f02dd
mingodad <mingodad@gmail.com>
2012-12-31 20:09:36 +0000

    After added a new member to SQRegFunction that indicates if a method is static, forgot to commit the change on the new api function that uses it.

--- squirrel/sqapi.cpp
+++ squirrel/sqapi.cpp
@@ -1693,7 +1693,7 @@ void sq_insert_reg_funcs(HSQUIRRELVM sqvm, SQRegFunction *obj_funcs){
 		sq_newclosure(sqvm,f.f,0);
 		sq_setparamscheck(sqvm,f.nparamscheck,f.typemask);
 		sq_setnativeclosurename(sqvm,-1,f.name);
-		sq_newslot(sqvm,-3,SQFalse);
+		sq_newslot(sqvm,-3,f.isStatic);
 		i++;
 	}
 }
