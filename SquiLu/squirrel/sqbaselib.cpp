dbf7801d72e8e434bc7d35e314f1c20e246bf797
mingodad <mingodad@gmail.com>
2016-03-21 23:08:31 +0000

    Fix conversion to bases biggeer or equal 14 to integer

--- SquiLu/squirrel/sqbaselib.cpp
+++ SquiLu/squirrel/sqbaselib.cpp
@@ -20,10 +20,11 @@ bool str2num(const SQChar *s,SQObjectPtr &res, SQInteger base=10)
 	SQChar *end;
 	const SQChar *e = s;
 	SQBool isfloat = SQFalse;
+	SQBool isEIntBase = base > 13; //to fix error converting hexadecimals with e like 56f0791e
 	SQChar c;
 	while((c = *e) != _SC('\0'))
 	{
-		if(c == _SC('.') || c == _SC('E')|| c == _SC('e')) { //e and E is for scientific notation
+		if(c == _SC('.') || (!isEIntBase && (c == _SC('E')|| c == _SC('e')))) { //e and E is for scientific notation
 			isfloat = SQTrue;
 			break;
 		}
